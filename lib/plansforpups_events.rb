30eae25e5d5a588b1af17693367c0e474fdf0102
Gina Graci <ginagraci92@gmail.com>
2019-12-05 04:32:15 +0000

    first commit

---
+++ lib/plansforpups_events.rb
@@ -0,0 +1,6 @@
+require "plansforpups_events/version"
+
+module PlansforpupsEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/plansforpups_events/version.rb
@@ -0,0 +1,3 @@
+module PlansforpupsEvents
+  VERSION = "0.1.0"
+end
