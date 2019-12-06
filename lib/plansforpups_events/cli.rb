15634ec0a01c5869bad91591e3e903b44aecd339
Gina Graci <ginagraci92@gmail.com>
2019-12-06 03:48:07 +0000

    building out list events and menu in cli folder

--- lib/plansforpups_events/cli.rb
+++ lib/plansforpups_events/cli.rb
@@ -2,7 +2,19 @@ class PlansforpupsEvents::CLI
   
   
   def start 
-    puts "sup"
+    puts "Hello, and welcome to the Plans for Pups Events gem!"
+    self.list_events
+    self.menu
   end 
+  
+  def self.list_events
+    #events = PlansforpupsEvents::Scraper
+    events.each_with_index(1) do |event, index|
+      puts "#{index}. #{event.name}: #{event.time} - #{event.location}"
+    end
+  end
+  
+  def self.menu
+  end
   
 end
