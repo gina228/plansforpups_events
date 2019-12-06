98475c522cf8445c95decc6e31232db768af8b91
Gina Graci <ginagraci92@gmail.com>
2019-12-06 04:33:42 +0000

    bundle install gems for cli file

--- lib/plansforpups_events/cli.rb
+++ lib/plansforpups_events/cli.rb
@@ -1,9 +1,14 @@
+require 'open-uri'
+require 'nokogiri'
+
 class PlansforpupsEvents::CLI
   
   @@all = []
   
   def start 
     puts "Hello, and welcome to the Plans for Pups Events gem!"
+    binding.pry
+    #events = scrape events
     events.each {|event| @@all << event}
     self.list_events
     self.menu
@@ -23,7 +28,17 @@ class PlansforpupsEvents::CLI
   
   def self.menu
     puts "Type in the number that corresponds with the event to get more information"
-    input = gets.strip.downcase
+    input = gets.strip
+    index = input.to_i - 1
+    if index > 0 && index < @@all.size
+    end
+  
+  
+    #meetup url is event.url
+    #check input is number
+    
+    #check input is in range / valid 
+    #scrape @@all[input-1].url
   end
   
 end
